3d0c8671e6465a033cdc9dc63fa9013543c51672
Petru-Armand Bancila <petrubancila@gmail.com>
2017-12-07 18:47:14 +0000

    Add solution for day 5 part 1

---
+++ five1.py
@@ -0,0 +1,25 @@
+def f(l):
+    l = list(map(int, l.split("\n")))
+    print(l)
+    jump = 0
+    count = -1
+    while True:
+        if jump < 0: jump -= 1
+        count += 1
+        pos = jump
+        l[jump] += 1
+        jump = l[jump] - 1 + pos
+        if jump >= len(l):
+            count += 1
+            break
+    return count
+
+text = open("day5input", "r").read()[:-1]
+
+# print(f(text))
+test = """0
+3
+0
+1
+-3"""
+print(f(text))
